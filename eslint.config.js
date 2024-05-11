e16d30a6bbf92a74477d68cb184f91ea92396dcb
DESKTOP-20BACJI\Admin <binh.ngv.dev@gmail.com>
2024-05-12 02:47:37 +0700

    ok

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,39 @@
+
+import tseslint from "typescript-eslint"
+import esLintReact from "eslint-plugin-react"
 import globals from "globals"
-import babelEslintParser from "@babel/eslint-parser"
-import esLintReactPlugin from "eslint-plugin-react"
-export default [
-    {
-        plugins: {
-            react: esLintReactPlugin,
-        },
-        files: ["**/*.ts", "**/*.tsx", "**/*.js","**/*.jsx"],
-        rules: {
-            "no-unused-vars": "warn",
-            "no-undef": "warn",
-            "quotes": [2, "double", { "avoidEscape": true, "allowTemplateLiterals": true }],
-            "no-unused-vars": "error",
-            "indent": [2, 4],
-            "react/jsx-uses-react": "error",
-            "react/jsx-uses-vars": "error",
-        },
-        languageOptions: {
-            parser: babelEslintParser,
-            globals: {...globals.node}
-        },
-        ignores: [
-            "public/**",
-        ]
+
+export default tseslint.config({
+    files: ["**/*.ts", "**/*.tsx", "**/*.js","**/*.jsx"],
+    extends: [
+        ...tseslint.configs.recommended,
+    ],
+    plugins: {
+        react: esLintReact
     },
-];
+    rules: {
+        "no-undef": "warn",
+        "quotes": [2, "double", { "avoidEscape": true, "allowTemplateLiterals": true }],
+        "no-unused-vars": "error",
+        "indent": [2, 4],
+    },
+    ignores: [
+        "public/**",
+    ],
+    languageOptions: {
+        parserOptions: {
+            ecmaFeatures: {
+                jsx: true,
+                
+            },
+        },
+        
+        globals: {
+            console: "readonly",
+            document: "writable",
+            React: "readonly",
+            ...globals.browser
+
+        }
+    }
+})

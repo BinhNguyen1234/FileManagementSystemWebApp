2bbf67397beedc08303972983501b81371ac45bb
DESKTOP-20BACJI\Admin <binh.ngv.dev@gmail.com>
2024-05-14 21:47:30 +0700

    ok

--- eslint.config.js
+++ eslint.config.js
@@ -1,35 +1,56 @@
+import reactEsLintRecommend from "eslint-plugin-react/configs/recommended.js";
+import globals from "globals";
+import stylisticJs from "@stylistic/eslint-plugin-js";
 
-import tseslint from "typescript-eslint"
-import globals from "globals"
-import stylisticJs from '@stylistic/eslint-plugin-js'
-
-export default tseslint.config({
-    files: ["**/*.ts", "**/*.tsx", "**/*.js","**/*.jsx"],
-    plugins: {
-        '@stylistic/js': stylisticJs
+export default [
+    {
+        ...reactEsLintRecommend,
     },
-    rules: {
-        "no-undef": "warn",
-        "@stylistic/js/quotes": [2, "double", { "avoidEscape": true, "allowTemplateLiterals": true }],
-        "no-unused-vars": "error",
-        "indent": [2, 4],
-    },
-    ignores: [
-        "public/**",
-    ],
-    languageOptions: {
-        parserOptions: {
-            ecmaFeatures: {
-                jsx: true,
+    {
+        files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
+        plugins: {
+            "@stylistic/js": stylisticJs,
+        },
+        rules: {
+            "no-undef": "warn",
+            "@stylistic/js/quotes": [
+                2,
+                "double",
+                { avoidEscape: true, allowTemplateLiterals: true },
+            ],
+            "no-unused-vars": "error",
+            indent: [2, 4],
+        },
+        languageOptions: {
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+            globals: {
+                console: "readonly",
+                document: "writable",
+                React: "readonly",
+                ...globals.browser,
             },
         },
-        
-        globals: {
-            console: "readonly",
-            document: "writable",
-            React: "readonly",
-            ...globals.browser
-
+        settings: {
+            react: {
+                createClass: "createReactClass", // Regex for Component Factory to use,
+                // default to "createReactClass"
+                pragma: "React", // Pragma to use, default to "React"
+                fragment: "Fragment", // Fragment to use (may be a property of <pragma>), default to "Fragment"
+                version: "detect", // React version. "detect" automatically picks the version you have installed.
+                flowVersion: "0.53",
+            },
+        },
+    },
+    { 
+        ignores: ["public/**"],
+    },
+    {
+        rules: {
+            "react/jsx-no-undef": ["error", { "allowGlobals": true }]
         }
     }
-})
+];
